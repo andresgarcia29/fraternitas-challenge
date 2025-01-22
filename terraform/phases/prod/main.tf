e65eddc79348572acb3e97b787fb5f511a188005
Andres Garcia <jose.andres.gm29@gmail.com>
2025-01-22 13:08:51 -0600

    feat(github-action): add gha keys

--- terraform/phases/prod/main.tf
+++ terraform/phases/prod/main.tf
@@ -12,25 +12,25 @@ module "s3" {
   }
 }
 
-# module "lambda" {
-#   source = "../../modules/aws_lambda"
+module "lambda" {
+  source = "../../modules/aws_lambda"
 
-#   function_name = local.name
-#   handler       = "main.lambda_handler"
-#   runtime       = "python3.13"
-#   s3 = {
-#     enabled     = true
-#     bucket_name = module.s3.bucket_name
-#     key         = "${local.name}-${var.app_version}.zip"
-#   }
-#   environment = {
-#     SERVICE_NAME = "fraternitas"
-#   }
+  function_name = local.name
+  handler       = "main.lambda_handler"
+  runtime       = "python3.13"
+  s3 = {
+    enabled     = true
+    bucket_name = module.s3.bucket_name
+    key         = "${local.name}-${var.app_version}.zip"
+  }
+  environment = {
+    SERVICE_NAME = "fraternitas"
+  }
 
-#   tags = {
-#     name    = local.name
-#     service = local.name
-#     env     = local.environment
-#     version = local.version
-#   }
-# }
+  tags = {
+    name    = local.name
+    service = local.name
+    env     = local.environment
+    version = local.version
+  }
+}
