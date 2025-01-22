6b50cf54c33709c94d2bd93d6a1e504f4dc955e4
Andres Garcia <jose.andres.gm29@gmail.com>
2025-01-22 14:51:17 -0600

    feat(python): Remove json dumps

--- api/main.py
+++ api/main.py
@@ -1,9 +1,6 @@
-import json
-
-
 def lambda_handler(event, context):
     """
     Entry point for the API Gateway.
     """
 
-    return json.dumps({"statusCode": 200, "message": "Hello, World!", "try": "7"})
+    return {"statusCode": 200, "message": "Hello, World!", "try": "7"}
